526881c6fef6b1b5df3bfbb93757cd156e6e5b41
Jun Hong <junhoongtan@gmail.com>
2024-05-20 11:16:49 +0800

    enhance rppg session provider

--- lib/models/rppg_session.dart
+++ lib/models/rppg_session.dart
@@ -1,17 +1,67 @@
 import 'package:binah_flutter_sdk/session/session.dart';
 import 'package:binah_flutter_sdk/session/session_builder/face_session_builder.dart';
+import 'package:binah_flutter_sdk/session/session_state.dart';
+import 'package:binah_flutter_sdk/vital_signs/vitals/stress_level.dart';
 import 'package:binah_poc/listeners/vital_sign_listener.dart';
 import 'package:binah_poc/listeners/image_data_listener.dart';
 import 'package:binah_poc/listeners/session_info_listener.dart';
+import 'package:binah_poc/models/binah_session.dart';
 import 'package:binah_poc/models/license/license_details.dart';
+import 'package:binah_poc/models/vital_sign/heart_rate.dart';
+import 'package:binah_poc/models/vital_sign/respiration_rate.dart';
+import 'package:binah_poc/models/vital_sign/spo2.dart';
+import 'package:binah_poc/models/vital_sign/stress_index.dart';
+import 'package:binah_poc/models/vital_sign/stress_level.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'rppg_session.g.dart';
 
 @riverpod
-Future<Session> createSession(CreateSessionRef ref) async =>
-    await FaceSessionBuilder()
+class RPpgSession extends _$RPpgSession {
+  @override
+  FutureOr<Session?> build() => null;
+
+  Future<Session> createNewSession() async {
+    return await FaceSessionBuilder()
         .withSessionInfoListener(BinahSessionListener(ref))
         .withImageDataListener(BinahImageListener(ref))
         .withVitalSignsListener(BinahVitalSignListener(ref))
         .build(ref.read(licenseDetailsProvider));
+  }
+
+  void start() async {
+    if (state.value == null ||
+        ref.read(binahSessionProvider) == SessionState.terminated ||
+        ref.read(binahSessionProvider) == SessionState.terminating) {
+      clearData();
+
+      state = await AsyncValue.guard(createNewSession);
+      if (!state.hasError) {
+        for (int i = 0; i < 10; i++) {
+          if (ref.read(binahSessionProvider) == SessionState.ready) {
+            break;
+          }
+          Future.delayed(const Duration(milliseconds: 500));
+        }
+      } else {
+        print('err');
+      }
+    }
+  }
+
+  void clearData() {
+    ref.read(respirationRateProvider.notifier).update(0);
+    ref.read(heartRateProvider.notifier).update(0);
+    ref.read(stressProvider.notifier).update(StressLevel.unknown);
+    ref.read(spo2Provider.notifier).update(0);
+    ref.read(stressIndexProvider.notifier).update(0);
+  }
+
+  Future<void> stop() async {
+    if (state.hasValue &&
+        (ref.read(binahSessionProvider) != SessionState.terminated ||
+            ref.read(binahSessionProvider) != SessionState.terminating)) {
+      state.value!.terminate();
+    }
+  }
+}
--- lib/models/rppg_session.g.dart
+++ lib/models/rppg_session.g.dart
@@ -6,20 +6,20 @@ part of 'rppg_session.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$createSessionHash() => r'de7d59428d548f09fe1abc70d72b7f4684d75c59';
+String _$rPpgSessionHash() => r'c7d5613df2c249a74c00e9dd3f478bb8eaa83648';
 
-/// See also [createSession].
-@ProviderFor(createSession)
-final createSessionProvider = AutoDisposeFutureProvider<Session>.internal(
-  createSession,
-  name: r'createSessionProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$createSessionHash,
+/// See also [RPpgSession].
+@ProviderFor(RPpgSession)
+final rPpgSessionProvider =
+    AutoDisposeAsyncNotifierProvider<RPpgSession, Session?>.internal(
+  RPpgSession.new,
+  name: r'rPpgSessionProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$rPpgSessionHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef CreateSessionRef = AutoDisposeFutureProviderRef<Session>;
+typedef _$RPpgSession = AutoDisposeAsyncNotifier<Session?>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
